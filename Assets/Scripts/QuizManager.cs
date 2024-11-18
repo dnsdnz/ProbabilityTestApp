453b3b5c8419019aa560f03c0e1e7793221e92ba
Deniz <berfz23@gmail.com>
2024-11-18 11:32:54 +0300

    edit

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -10,7 +10,15 @@ using UnityEngine.UI;
 public class QuizManager : MonoBehaviour
 {
     public GameObject introPanel;       
-    public TMP_InputField nameInputField; 
+    public TMP_InputField collectorNameInputField; 
+    public TMP_InputField childNameInputField; 
+    public TMP_InputField birthdayInputField; 
+    public TMP_InputField siblingCountInputField; 
+
+    public TMP_Dropdown genderDropdown;
+    public TMP_Dropdown motherEducationDropdown;
+    public TMP_Dropdown fatherEducationDropdown;
+
     public Button startButton;         
 
     [Serializable]
@@ -25,10 +33,35 @@ public class QuizManager : MonoBehaviour
     private int currentPanelIndex = 0;
     private float questionTimer = 5f; //TODO 90 sn yap
     private bool isAnswered = false;
+    private bool isQuestionActive = false;
     private List<int> results = new List<int>(); // correct1-wrong0
     private DatabaseReference databaseReference;
     
-    private string collectorName; // Kullanıcının adı
+    //Veri toplayan kişi----
+    //Uygulama tarihi----
+    //Çocuğun adı soyadı---
+    //Çocuğun Doğum Tarihi----
+    //Çocuğun cinsiyeti---
+    //Anne öğrenim durumu---
+    //Baba öğrenim durumu----
+    //Kardeş sayısı---
+    //Çocuğun yaşadığı il/ilçe/köy
+    //Daha önce okul öncesi eğitimi aldı mı
+    //Daha önce okul öncesi eğitimi alma süresi
+    //Uygulama başlama zamanı
+    //Uygulama bitiş zamanı
+    //Toplam oturum süresi
+    //TESTİN TAMAMINDAN ALINAN TOPLAM PUAN
+    //1... sorunun puanı
+    //1... BÖLÜM TOPLAM PUANI
+    
+    private string collectorName; 
+    private string childName;
+    private string birthday;
+    private string gender; 
+    private string motherEducation; 
+    private string fatherEducation; 
+    private int siblingCount; 
 
     void Start()
     {
@@ -58,7 +91,7 @@ public class QuizManager : MonoBehaviour
     
     void Update()
     {
-        if (currentPanelIndex < questionPanels.Count && !isAnswered)
+        if (isQuestionActive && !isAnswered)
         {
             questionTimer -= Time.deltaTime;
             if (questionTimer <= 0)
@@ -70,16 +103,16 @@ public class QuizManager : MonoBehaviour
     
     void OnStartButtonClicked()
     {
-        if (!string.IsNullOrEmpty(nameInputField.text))
-        {
-            collectorName = nameInputField.text;
-            introPanel.SetActive(false);
-            ShowPanel(0); 
-        }
-        else
-        {
-            Debug.LogWarning("Enter name.");
-        }
+        collectorName = collectorNameInputField.text;
+        childName = childNameInputField.text;
+        birthday = birthdayInputField.text;
+
+        gender = genderDropdown.options[genderDropdown.value].text; 
+        motherEducation = motherEducationDropdown.options[motherEducationDropdown.value].text; 
+        fatherEducation = fatherEducationDropdown.options[fatherEducationDropdown.value].text;
+
+        introPanel.SetActive(false);
+        ShowPanel(0);
     }
 
     void ShowPanel(int index)
@@ -106,6 +139,7 @@ public class QuizManager : MonoBehaviour
 
         questionTimer = 5f;
         isAnswered = false;
+        isQuestionActive = true;
     }
     
     void OnButtonClicked(Button clickedButton)
@@ -117,6 +151,7 @@ public class QuizManager : MonoBehaviour
     void RegisterAnswer(bool isCorrect)
     {
         isAnswered = true;
+        isQuestionActive = false;
         results.Add(isCorrect ? 1 : 0); 
         ShowPanel(currentPanelIndex + 1); 
     }
@@ -128,12 +163,12 @@ public class QuizManager : MonoBehaviour
         var currentDate = DateTime.Now.ToString("yyyy-MM-dd");
         string currentTime = DateTime.Now.ToString("HH:mm:ss");
                 
-        WriteData( collectorName,currentDate,"DenizC", "Kız", "Lise", "İlkokul",
-            1, "İST", "evet", "2 yıl",currentTime,
+        WriteData(collectorName, currentDate, childName, birthday, gender, motherEducation, fatherEducation,
+            siblingCount, "İST", "evet", "2 yıl",currentTime,
             currentTime, 10,results[0],results[1],1);
     }
     
-    void WriteData(string collectorName, string currentDate, string childName, string gender, string motherEducation, string fatherEducation,
+    void WriteData(string collectorName, string currentDate, string childName, string birthday, string gender, string motherEducation, string fatherEducation,
         int siblingCount, string location, string getEducationBefore, string getEducationBeforeTime, string appStartTime, string appEndTime,
         int completeScore, int q1Score, int q2Score, int chapter1Score)
     {
@@ -142,6 +177,7 @@ public class QuizManager : MonoBehaviour
             {"VeriToplayanKişi", collectorName},
             {"UygulamaTarihi", currentDate},
             {"ÇocuğunAdıSoyadı", childName},
+            {"ÇocuğunDoğumTarihi", birthday},
             {"ÇocuğunCinsiyeti", gender},
             {"AnneÖğrenimDurumu", motherEducation},
             {"BabaÖğrenimDurumu", fatherEducation},
